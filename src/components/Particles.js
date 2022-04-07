35479f4282a628e00004835dd01c2de2477bf546
Steve LeValley <slevalley@gmail.com>
2022-04-07 08:33:36 -0700

    Added tech icons to particles

--- src/components/Particles.js
+++ src/components/Particles.js
@@ -1,6 +1,15 @@
 import React from "react";
 import { Particles as ReactParticles } from "react-particles-js";
 import { makeStyles } from "@material-ui/core/styles";
+import javascriptIcon from '../images/javascriptIcon.svg'
+import pythonIcon from '../images/pythonIcon.svg'
+import reactIcon from '../images/reactIcon.svg'
+import postgresIcon from '../images/postgresIcon.svg'
+import dockerIcon from '../images/dockerIcon.svg'
+import htmlIcon from '../images/htmlIcon.svg'
+import cssIcon from '../images/cssIcon.svg'
+import nodeIcon from '../images/nodeIcon.svg'
+import githubIcon from '../images/githubIcon.svg'
 
 
 const useStyles = makeStyles((theme) => ({
@@ -19,25 +28,72 @@ const Particles = () => {
       params={{
         particles: {
           number: {
-            value: 50,
+            value: 35,
             density: {
               enable: true,
               value_area: 900,
             },
           },
           shape: {
-            type: "circle",
+            type: ["circle", "image"],
+            image: [
+              {
+                src: cssIcon,
+                height: 40,
+                width: 40
+              },
+              {
+                src: dockerIcon,
+                height: 40,
+                width: 40
+              },
+              {
+                src: githubIcon,
+                height: 40,
+                width: 40
+              },
+              {
+                src: htmlIcon,
+                height: 40,
+                width: 40
+              },
+              {
+                src: javascriptIcon,
+                height: 40,
+                width: 40
+              },
+              {
+                src: nodeIcon,
+                height: 40,
+                width: 40
+              },
+              {
+                src: reactIcon,
+                height: 40,
+                width: 40
+              },
+              {
+                src: postgresIcon,
+                height: 40,
+                width: 40
+              },
+              {
+                src: pythonIcon,
+                height: 40,
+                width: 40
+              }
+            ],
             stroke: {
-              width: 1,
+              width: 10,
               color: "tomato",
             },
           },
           size: {
-            value: 8,
+            value: 30,
             random: true,
             anim: {
               enable: false,
-              speed: 6,
+              speed: 1,
               size_min: 0,
               sync: true,
             },
@@ -48,7 +104,7 @@ const Particles = () => {
             anim: {
               enable: true,
               speed: 1,
-              opacity_min: 0.1,
+              opacity_min: 0.5,
               sync: false,
             },
           },
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import Particles from "./Particles";
+import ImageParticles from "./ImageParticles";
 
 const Home = () => {
   return (
