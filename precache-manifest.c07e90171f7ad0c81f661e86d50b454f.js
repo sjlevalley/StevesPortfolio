1e3a74efe8bd56a8827d4f3238463da275d876e8
Steve LeValley <slevalley@gmail.com>
2022-07-14 18:38:47 -0700

    Updates

---
+++ precache-manifest.c07e90171f7ad0c81f661e86d50b454f.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00c799d20f2760d3a809fc075ad2eb9d",
+    "url": "/mui-portfolio/index.html"
+  },
+  {
+    "revision": "8c13cb7aeae84d6ec5cd",
+    "url": "/mui-portfolio/static/css/main.c35d1ac8.chunk.css"
+  },
+  {
+    "revision": "2dc610e1210254fe8df0",
+    "url": "/mui-portfolio/static/js/2.dc4bd0ea.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/mui-portfolio/static/js/2.dc4bd0ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c13cb7aeae84d6ec5cd",
+    "url": "/mui-portfolio/static/js/main.9c0f0da5.chunk.js"
+  },
+  {
+    "revision": "95b8a550b2ecb82980d4",
+    "url": "/mui-portfolio/static/js/runtime-main.021520e5.js"
+  },
+  {
+    "revision": "fd1b543d0c8e0ba4007f93a90fd98ecc",
+    "url": "/mui-portfolio/static/media/AmazonCloneScreenshot.fd1b543d.png"
+  },
+  {
+    "revision": "8148ab98a6a1263f701b5cc33f4c1cb1",
+    "url": "/mui-portfolio/static/media/StevesFineDining.8148ab98.png"
+  },
+  {
+    "revision": "7c8e5f50725885d51b72b45164274e41",
+    "url": "/mui-portfolio/static/media/avatar.7c8e5f50.png"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/mui-portfolio/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "9c9c21e82f7cca1404006ede8add5ecb",
+    "url": "/mui-portfolio/static/media/cssIcon.9c9c21e8.svg"
+  },
+  {
+    "revision": "353d2c749e4645919dc19fbf8a083678",
+    "url": "/mui-portfolio/static/media/dockerIcon.353d2c74.svg"
+  },
+  {
+    "revision": "a6d8c48cdc66316c579ced5a55cb88ac",
+    "url": "/mui-portfolio/static/media/githubIcon.a6d8c48c.svg"
+  },
+  {
+    "revision": "09ce7b6e207fb4a6d7d30743475368bd",
+    "url": "/mui-portfolio/static/media/htmlIcon.09ce7b6e.svg"
+  },
+  {
+    "revision": "87711407412494d529d45ee76167583c",
+    "url": "/mui-portfolio/static/media/javascriptIcon.87711407.svg"
+  },
+  {
+    "revision": "3a7e1b9540d19f8a61acdd7c7ca7e526",
+    "url": "/mui-portfolio/static/media/nodeIcon.3a7e1b95.svg"
+  },
+  {
+    "revision": "60d45ba45a24df23755ae585db1d089a",
+    "url": "/mui-portfolio/static/media/postgresIcon.60d45ba4.svg"
+  },
+  {
+    "revision": "573a25d285cf7f3c393946afb21baa07",
+    "url": "/mui-portfolio/static/media/pythonIcon.573a25d2.svg"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "/mui-portfolio/static/media/react-redux.a8b012a0.jpg"
+  },
+  {
+    "revision": "f370c1e0adb80194dd3ea10e468bc69a",
+    "url": "/mui-portfolio/static/media/react.f370c1e0.png"
+  },
+  {
+    "revision": "3df930dfd92a3d0729be0f9cb01c49d1",
+    "url": "/mui-portfolio/static/media/reactIcon.3df930df.svg"
+  }
+]);
