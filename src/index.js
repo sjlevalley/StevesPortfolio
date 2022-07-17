34410990902ef7d4c9bd90c9d36945829b39adf7
Steve LeValley <slevalley@gmail.com>
2022-07-17 13:58:56 -0700

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App.jsx";
 import { BrowserRouter as Router } from 'react-router-dom'
+import App from "./App.jsx";
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById("root"));
+ReactDOM.render(<Router basename="/mui-portfolio"><App /></Router>, document.getElementById("root"));
